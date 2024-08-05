7cd96dedd44ec816de04e4d6de08e00b7e1d9fac
AoiTuNa <bestspark123@naver.com>
2024-08-05 11:01:27 +0900

    feat/lifecycle 실습

--- src/main/java/com/nhnacademy/practice/HelloServlet.java
+++ src/main/java/com/nhnacademy/practice/HelloServlet.java
@@ -1,30 +1,54 @@
 package com.nhnacademy.practice;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.util.logging.Logger;
 
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private String message;
 
-    public void init() {
-        message = "Hello World!";
-    }
+    private static Logger log = Logger.getLogger(HelloServlet.class.getName());
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
-    }
-
+    @Override
     public void destroy() {
+        log.info("before destroy!");
+        super.destroy();
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        log.info("before init!");
+        super.init(config);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setCharacterEncoding("utf-8");
+        try(PrintWriter writer = resp.getWriter()) {
+            writer.println("<!DOCTYPE html>");
+            writer.println("<html>");
+            writer.println("<head>");
+            writer.println("<meta charset='utf-8'>");
+            writer.println("</head>");
+            writer.println("<body>");
+            writer.println("<h1>hello servlet!</h1>");
+            writer.println("<h1>안녕 서블릿!</h1>");
+            writer.println("</body>");
+            writer.println("</html>");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.info("before service!");
+        super.service(req, resp);
     }
 }
